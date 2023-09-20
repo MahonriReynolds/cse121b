e28afb02262983b9c788661811a01245d5f25258
MahonriReynolds <mahonri.r66@gmail.com>
2023-09-20 17:25:43 -0600

    w02 first activity

---
+++ week02/dom-basics.js
@@ -0,0 +1,19 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+
+const my_img = document.createElement("img");
+my_img.src = "https://picsum.photos/200";
+my_img.alt = 'Random Photo From Picsum'
+document.body.appendChild(my_img);
+
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+
+const my_section = document.createElement("section");
+my_section.innerHTML = "<h2>CSE 121b</h2><p>Welcome to Javascript Language</p>";
+document.body.appendChild(my_section);
